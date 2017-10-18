1e532a88014d03b72b6b43f13018c6418f698ef2
jborbely <joe.borbely@gmail.com>
2017-10-18 18:16:54 +1300

    v0.3.2 release

--- msl/loadlib/__init__.py
+++ msl/loadlib/__init__.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 
 __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017, ' + __author__
-__version__ = '0.3.2.dev0'
+__version__ = '0.3.2'
 
 version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
