0cf1714b4149ac87c494f35844f42009fc7c0661
jborbely <joe.borbely@gmail.com>
2018-09-10 17:00:14 +1200

    update the __version__ regex search pattern

--- msl/loadlib/__init__.py
+++ msl/loadlib/__init__.py
@@ -11,7 +11,7 @@ __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017 - 2018, ' + __author__
 __version__ = '0.4.2.dev0'
 
-_v = re.search('(\d+).(\d+).(\d+)\.?(.*)', __version__).groups()
+_v = re.search('(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
 
 version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
