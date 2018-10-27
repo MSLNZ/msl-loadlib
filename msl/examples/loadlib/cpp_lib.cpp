5ebe9aeaf28b7a80c29a88db9455d2dba243d575
jborbely <joe.borbely@gmail.com>
2018-10-27 16:09:30 +1300

    recompile cpp_lib64.so to include the struct examples

--- msl/examples/loadlib/cpp_lib.cpp
+++ msl/examples/loadlib/cpp_lib.cpp
@@ -2,7 +2,7 @@
 // Examples that show how to pass various data types between Python and a C++ library.
 //
 // Compiled using:
-//   g++ cpp_lib.cpp -fPIC -static -shared -o cpp_lib64.so
+//   g++ -c cpp_lib.cpp -fPIC -static -shared -o cpp_lib64.so
 //
 #include <math.h>
 #include "cpp_lib.h"
