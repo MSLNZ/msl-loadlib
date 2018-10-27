5f688a2d9bc006f8a517ec2b881df1d1e906e1b3
jborbely <joe.borbely@gmail.com>
2018-10-27 16:36:40 +1300

    remove the .dll extension so that the test works in linux

--- tests/uñicödé/cpp32unicode.py
+++ tests/uñicödé/cpp32unicode.py
@@ -7,7 +7,7 @@ from msl.loadlib import Server32
 class Cpp32(Server32):
 
     def __init__(self, host, port, quiet, **kwargs):
-        super(Cpp32, self).__init__(u'cpp_lib32-uñicödé.dll', 'cdll', host, port, quiet)
+        super(Cpp32, self).__init__(u'cpp_lib32-uñicödé', 'cdll', host, port, quiet)
 
     def add(self, a, b):
         return self.lib.add(ctypes.c_int32(a), ctypes.c_int32(b))
