20c515d546605f0c7ea2ce8bc7986a56409168bb
jborbely <joe.borbely@gmail.com>
2019-01-04 12:06:43 +1300

    use Python 3 for intersphinx_mapping

--- docs/conf.py
+++ docs/conf.py
@@ -208,7 +208,7 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/{}'.format(sys.version_info.major), None),
+    'python': ('https://docs.python.org/3', None),
     'py4j': ('https://www.py4j.org/', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
 }
