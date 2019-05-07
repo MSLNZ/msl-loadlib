b98248e7e8845291cd98698d10b0db9fc0e056d7
jborbely <joe.borbely@gmail.com>
2019-05-07 16:08:03 +1200

    bump v0.6.0 -> v0.6.1.dev0

--- msl/loadlib/__init__.py
+++ msl/loadlib/__init__.py
@@ -9,11 +9,11 @@ from collections import namedtuple
 
 __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017 - 2019, ' + __author__
-__version__ = '0.6.0'
+__version__ = '0.6.1.dev0'
 
 _v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
 
-version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), 'final')
+version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
 
 IS_WINDOWS = sys.platform in ['win32', 'cygwin']
