1bd1b5e621099d16e82def9a4ea87e337e6aa87b
jborbely <joe.borbely@gmail.com>
2020-02-07 16:16:09 +1300

    add "of New Zealand" to latex_documents

--- docs/conf.py
+++ docs/conf.py
@@ -160,7 +160,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'MSL-LoadLib.tex', 'MSL-LoadLib Documentation',
-     'Measurement Standards Laboratory', 'manual'),
+     'Measurement Standards Laboratory of New Zealand', 'manual'),
 ]
 
 
