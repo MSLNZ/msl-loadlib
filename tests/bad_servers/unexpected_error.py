cd1647200ba457206ab917724eea9718caf5c860
jborbely <joe.borbely@gmail.com>
2020-11-13 11:32:25 +1300

    raise the ZeroDivisionError before calling super()

--- tests/bad_servers/unexpected_error.py
+++ tests/bad_servers/unexpected_error.py
@@ -6,8 +6,8 @@ from msl.loadlib import Server32
 class UnexpectedError(Server32):
 
     def __init__(self, host, port, quiet, **kwargs):
+        # any error would be fine
+        x = 1/0
+
         path = os.path.join(os.path.dirname(__file__), '..', '..', 'msl', 'examples', 'loadlib', 'cpp_lib32')
         super(UnexpectedError, self).__init__(os.path.abspath(path), 'cdll', host, port, quiet, **kwargs)
-
-        # any unexpected error would be fine
-        x = 1/0
