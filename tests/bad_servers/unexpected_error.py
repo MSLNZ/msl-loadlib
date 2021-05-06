3cbc6598458280f7c512f1980484a54aeb14085d
jborbely <joe.borbely@gmail.com>
2021-05-06 13:17:54 +1200

    remove the code after the ZeroDivisionError is raised

--- tests/bad_servers/unexpected_error.py
+++ tests/bad_servers/unexpected_error.py
@@ -1,13 +1,9 @@
-import os
-
 from msl.loadlib import Server32
 
 
 class UnexpectedError(Server32):
 
     def __init__(self, host, port, **kwargs):
+
         # any error would be fine
         x = 1/0
-
-        path = os.path.join(os.path.dirname(__file__), '..', '..', 'msl', 'examples', 'loadlib', 'cpp_lib32')
-        super(UnexpectedError, self).__init__(os.path.abspath(path), 'cdll', host, port, **kwargs)
