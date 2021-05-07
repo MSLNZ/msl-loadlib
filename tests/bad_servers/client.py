6b1cf995116ed060679477c0496e98836a05e6af
jborbely <joe.borbely@gmail.com>
2021-05-07 13:01:48 +1200

    decrease timeout to 2 seconds for bad-server tests

--- tests/bad_servers/client.py
+++ tests/bad_servers/client.py
@@ -4,4 +4,4 @@ from msl.loadlib import Client64
 class Client(Client64):
 
     def __init__(self, module32):
-        super(Client, self).__init__(module32, timeout=5)
+        super(Client, self).__init__(module32, timeout=2)
