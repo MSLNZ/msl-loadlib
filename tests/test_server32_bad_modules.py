5672c0acf909f66812c618dfe625447ef1a3a808
jborbely <joe.borbely@gmail.com>
2021-05-07 13:01:27 +1200

    improve buggy ConnectionRefusedError issue in bad-server tests

--- tests/test_server32_bad_modules.py
+++ tests/test_server32_bad_modules.py
@@ -11,55 +11,63 @@ from client import Client
 skipif_macos = pytest.mark.skipif(IS_MAC, reason='the 32-bit server for macOS does not exist')
 
 
+def check(module_name, match):
+    # This test module is a bit buggy.
+    # Sometimes we get a ConnectionRefusedError that we want to ignore.
+    attempts = 1
+    max_attempts = 3
+    while True:
+        try:
+            with pytest.raises(ConnectionTimeoutError, match=match):
+                Client(module_name)
+        except ConnectionRefusedError:
+            if attempts == max_attempts:
+                raise
+            attempts += 1
+        else:
+            break  # then this test was successful
+
+
 @skipif_macos
 def test_no_server32_subclass():
-    with pytest.raises(ConnectionTimeoutError, match=r'Module does not contain a class that is a subclass of Server32'):
-        Client('no_server32_subclass')
+    check('no_server32_subclass', r'Module does not contain a class that is a subclass of Server32')
 
 
 @skipif_macos
 def test_no_init():
-    with pytest.raises(ConnectionTimeoutError, match=r'class NoInit\(Server32\):'):
-        Client('no_init')
+    check('no_init', r'class NoInit\(Server32\):')
 
 
 @skipif_macos
 def test_bad_init_args():
-    with pytest.raises(ConnectionTimeoutError, match=r'class BadInitArgs\(Server32\):'):
-        Client('bad_init_args')
+    check('bad_init_args', r'class BadInitArgs\(Server32\):')
 
 
 @skipif_macos
 def test_no_super():
-    with pytest.raises(ConnectionTimeoutError, match=r'class NoSuper\(Server32\):'):
-        Client('no_super')
+    check('no_super', r'class NoSuper\(Server32\):')
 
 
 @skipif_macos
 def test_bad_super_init():
-    with pytest.raises(ConnectionTimeoutError, match=r'class BadSuperInit\(Server32\):'):
-        Client('bad_super_init')
+    check('bad_super_init', r'class BadSuperInit\(Server32\):')
 
 
 @skipif_macos
 def test_bad_lib_path():
-    with pytest.raises(ConnectionTimeoutError, match=r"Cannot find 'doesnotexist' for libtype='cdll'"):
-        Client('bad_lib_path')
+    check('bad_lib_path', r"Cannot find 'doesnotexist' for libtype='cdll'")
 
 
 @skipif_macos
 def test_bad_lib_type():
-    with pytest.raises(ConnectionTimeoutError, match=r"Cannot load libtype='invalid'"):
-        Client('bad_lib_type')
+    check('bad_lib_type', r"Cannot load libtype='invalid'")
 
 
 @skipif_macos
 def test_unexpected_error():
-    with pytest.raises(ConnectionTimeoutError, match=r'ZeroDivisionError'):
-        Client('unexpected_error')
+    check('unexpected_error', r'ZeroDivisionError')
 
 
 @skipif_macos
 def test_wrong_bitness():
-    with pytest.raises(ConnectionTimeoutError, match=r'Failed to load'):
-        Client('wrong_bitness')
+    check('wrong_bitness', r'Failed to load')
