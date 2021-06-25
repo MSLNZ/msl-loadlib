503ecc76fffcecf52c8fb081a9252817a791dcc3
jborbely <joe.borbely@gmail.com>
2021-06-25 16:07:43 +1200

    create test_buffer_size_4096

--- tests/test_server32_stdout_stderr.py
+++ tests/test_server32_stdout_stderr.py
@@ -16,27 +16,54 @@ else:
 
 class Print32(Server32):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, **kwargs):
         path = os.path.join(Server32.examples_dir(), 'cpp_lib32')
         super(Print32, self).__init__(path, 'cdll', host, port)
 
-        print('this is a message')
-        print('there is a problem', file=sys.stderr)
+        if kwargs['show'] == 'True':
+            print('this is a message')
+            print('there is a problem', file=sys.stderr)
+
+    def write(self, n, stdout):
+        stream = sys.stdout if stdout else sys.stderr
+        print('x'*n, end='', file=stream)
+        return True
 
 
 class Print64(Client64):
 
-    def __init__(self):
-        super(Print64, self).__init__(__file__)
+    def __init__(self, show):
+        super(Print64, self).__init__(__file__, show=show)
+
+    def write(self, n, stdout):
+        return self.request32('write', n, stdout)
 
 
 @skipif_no_server32
 def test_shutdown_server32():
-
-    p = Print64()
+    p = Print64(True)
     stdout, stderr = p.shutdown_server32()
     assert stdout.read().rstrip() == b'this is a message'
     assert stderr.read().rstrip() == b'there is a problem'
+
+    # calling shutdown_server32 multiple times is okay
+    # but the buffer has been read already
+    for _ in range(10):
+        stdout, stderr = p.shutdown_server32()
+        assert not stdout.read()
+        assert not stderr.read()
+
+
+@skipif_no_server32
+def test_buffer_size_4096():
+    # 4096 is the maximum buffer size for a PIPE before the 32-bit server blocks
+    n = 4096
+    p = Print64(False)
+    assert p.write(n, True)
+    assert p.write(n, False)
+    stdout, stderr = p.shutdown_server32()
+    assert stdout.read() == b'x'*n
+    assert stderr.read() == b'x'*n
 
     # calling shutdown_server32 multiple times is okay
     # but the buffer has been read already
