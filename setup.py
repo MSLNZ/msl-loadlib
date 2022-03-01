3781e7789390197077bbdedfef006ecbecd37d17
jborbely <joe.borbely@gmail.com>
2022-03-02 10:43:11 +1300

    add support for Python 3.10

--- setup.py
+++ setup.py
@@ -218,6 +218,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
     ],
