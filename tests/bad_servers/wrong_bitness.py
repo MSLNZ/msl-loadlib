bd9189dd6d5ee242ed073a0dbd7319bff333a279
jborbely <joe.borbely@gmail.com>
2022-10-27 18:57:17 +1300

    fix spelling mistake

--- tests/bad_servers/wrong_bitness.py
+++ tests/bad_servers/wrong_bitness.py
@@ -3,8 +3,8 @@ import os
 from msl.loadlib import Server32
 
 
-class WringBitness(Server32):
+class WrongBitness(Server32):
 
     def __init__(self, host, port, **kwargs):
         path = os.path.join(os.path.dirname(__file__), '..', '..', 'msl', 'examples', 'loadlib', 'cpp_lib64')
-        super(WringBitness, self).__init__(os.path.abspath(path), 'cdll', host, port, **kwargs)
+        super(WrongBitness, self).__init__(os.path.abspath(path), 'cdll', host, port, **kwargs)
