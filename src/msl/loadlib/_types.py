505d4923011a27f93fc4e64a2dbecc85764c9e7d
jborbely <joe.borbely@gmail.com>
2025-04-30 20:08:24 +1200

    simplify PathLike for Python 3.10+

--- src/msl/loadlib/_types.py
+++ src/msl/loadlib/_types.py
@@ -3,15 +3,10 @@
 from __future__ import annotations
 
 import os
-import sys
 from typing import Literal
 
 LibType = Literal["cdll", "windll", "oledll", "net", "clr", "java", "com", "activex"]
 """Supported library types."""
 
-if sys.version_info[:2] > (3, 9):
-    PathLike = str | bytes | os.PathLike[str] | os.PathLike[bytes]
-    """A [path-like object][]{:target="_blank"}."""
-else:
-    from typing import Union  # pyright: ignore[reportDeprecated]
-    PathLike = Union[str, bytes, os.PathLike]  # pyright: ignore[reportMissingTypeArgument, reportDeprecated]
+PathLike = str | bytes | os.PathLike[str] | os.PathLike[bytes]
+"""A [path-like object][]{:target="_blank"}."""
