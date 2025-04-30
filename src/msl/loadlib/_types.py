425c82f9dd4fb91208e36605a3e8193c886f12bf
jborbely <joe.borbely@gmail.com>
2025-04-30 15:47:40 +1200

    fix PathLike for Python < 3.10

--- src/msl/loadlib/_types.py
+++ src/msl/loadlib/_types.py
@@ -3,10 +3,15 @@
 from __future__ import annotations
 
 import os
+import sys
 from typing import Literal
 
 LibType = Literal["cdll", "windll", "oledll", "net", "clr", "java", "com", "activex"]
 """Supported library types."""
 
-PathLike = str | bytes | os.PathLike[str] | os.PathLike[bytes]
-"""A [path-like object][]{:target="_blank"}."""
+if sys.version_info[:2] > (3, 9):
+    PathLike = str | bytes | os.PathLike[str] | os.PathLike[bytes]
+    """A [path-like object][]{:target="_blank"}."""
+else:
+    from typing import Union  # pyright: ignore[reportDeprecated]
+    PathLike = Union[str, bytes, os.PathLike]  # pyright: ignore[reportMissingTypeArgument, reportDeprecated]
