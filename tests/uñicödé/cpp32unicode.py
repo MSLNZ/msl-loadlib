f06f9c5ab4ee3e42359a1722b49098e8d5ed6493
jborbely <joe.borbely@gmail.com>
2025-05-03 09:47:13 +1200

    fmt/lint cpp32unicode.py test file

--- tests/uñicödé/cpp32unicode.py
+++ tests/uñicödé/cpp32unicode.py
@@ -4,8 +4,9 @@ from msl.loadlib import Server32
 
 
 class Cpp32(Server32):
-    def __init__(self, host, port, **kwargs):
+    def __init__(self, host: str, port: int) -> None:
         super().__init__("cpp_lib32-uñicödé", "cdll", host, port)
 
-    def add(self, a, b):
-        return self.lib.add(ctypes.c_int32(a), ctypes.c_int32(b))
+    def add(self, a: int, b: int) -> int:
+        result: int = self.lib.add(ctypes.c_int32(a), ctypes.c_int32(b))
+        return result
