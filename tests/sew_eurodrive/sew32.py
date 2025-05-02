b6d7d840d93f6c2ea69ce05c1e97d1d43f499cf0
jborbely <joe.borbely@gmail.com>
2025-05-03 09:43:27 +1200

    fmt/lint SEWEuroDrive test

--- tests/sew_eurodrive/sew32.py
+++ tests/sew_eurodrive/sew32.py
@@ -1,14 +1,15 @@
-import os
+from pathlib import Path
 
 from msl.loadlib import Server32
 
 
 class SEWEuroDrive32(Server32):
-    def __init__(self, host, port, **kwargs):
-        path = os.path.join(os.path.dirname(__file__), "FirstDll.dll")
+    def __init__(self, host: str, port: int) -> None:
+        path = Path(__file__).parent / "FirstDll.dll"
         super().__init__(path, "clr", host, port)
-        self.first_class = self.lib.FirstDll.FirstClass()
+        self.first_class = self.lib.FirstDll.FirstClass()  # pyright: ignore[reportUnannotatedClassAttribute]
 
-    def do_something(self, value):
+    def do_something(self, value: float) -> float:
         # returns "value/3/2"
-        return self.first_class.DoSometing(value)  # cSpell: ignore Someting
+        result: float = self.first_class.DoSometing(value)  # cSpell: ignore Someting
+        return result
--- tests/test_server32_sew_eurodrive.py
+++ tests/test_server32_sew_eurodrive.py
@@ -1,15 +1,14 @@
-import os
+from pathlib import Path
 
-from conftest import skipif_not_windows
-from conftest import xfail_windows_ga
+from conftest import skipif_not_windows, xfail_windows_ga
 from msl.loadlib import Client64
 
 
 @xfail_windows_ga
 @skipif_not_windows
-def test_sew_eurodrive():
-    """
-    The reason for creating this test case was discussed through email.
+def test_sew_eurodrive() -> None:
+    """The reason for creating this test case was discussed through email.
+
     The LoadLibrary class has been modified so that clr.AddReference is
     called before clr.System.Reflection.Assembly.LoadFile is called.
     The order of execution was important for the example .NET DLL
@@ -17,11 +16,12 @@ def test_sew_eurodrive():
     """
 
     class SEWEuroDrive64(Client64):
-        def __init__(self):
-            super().__init__(os.path.join(os.path.dirname(__file__), "sew_eurodrive", "sew32.py"))
+        def __init__(self) -> None:
+            super().__init__(Path(__file__).parent / "sew_eurodrive" / "sew32.py")
 
-        def do_something(self, value):
-            return self.request32("do_something", value)
+        def do_something(self, value: float) -> float:
+            result: float = self.request32("do_something", value)
+            return result
 
-    sew = SEWEuroDrive64()
-    assert sew.do_something(600) == 100
+    with SEWEuroDrive64() as sew:
+        assert sew.do_something(600) == 100.0
