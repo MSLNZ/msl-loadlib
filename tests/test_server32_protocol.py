bfe406b9d9c5c8877fdd05813de3539f700447ff
jborbely <joe.borbely@gmail.com>
2025-05-03 08:11:42 +1200

    simplify test_protocol

--- tests/test_server32_protocol.py
+++ tests/test_server32_protocol.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 import os
-import re
-import sys
 from typing import Any
 
 from msl.loadlib import Client64, Server32
@@ -12,7 +10,7 @@ if Server32.is_interpreter():
 
     skipif_no_server32 = Mock()
 else:
-    from conftest import skipif_no_server32
+    from conftest import skipif_no_server32  # type: ignore[assignment]
 
 
 class Bounce32(Server32):
@@ -33,7 +31,7 @@ class Bounce64(Client64):
 
 
 @skipif_no_server32
-def test_protocol() -> None:
+def test_protocol() -> None:  # type: ignore[misc]
     import pytest
 
     args = (None, True, False, 1, -2.0, 5 - 6j, [1, [2.0, "hello"]], {"one": "1", "two": 2})
@@ -48,30 +46,12 @@ def test_protocol() -> None:
         "Dict": {"one": "1", "two": 2},
     }
 
-    # determine the maximum pickle protocol allowed based on
-    # Python versions that are used by the server and the client
-    server_version = re.match(r"Python\s(\d+)\.(\d+)", Server32.version())
-    assert server_version is not None
-    server_major, server_minor = map(int, server_version.groups())
-    client_major, client_minor = sys.version_info[:2]
-    major = min(server_major, client_major)
-    minor = min(server_minor, client_minor)
-    if major == 2:
-        max_protocol = 2
-    elif major == 3 and minor < 4:
-        max_protocol = 3
-    elif major == 3 and minor < 8:
-        max_protocol = 4
-    else:
-        max_protocol = 5
+    for protocol in [0, 1, 2, 3, 4, 5]:
+        with Bounce64(protocol) as b:
+            a, k = b.bounce(*args, **kwargs)
+            assert a == args
+            assert k == kwargs
 
-    for protocol in list(range(max_protocol + 2)):
-        if protocol > max_protocol:
-            with pytest.raises(ValueError, match=r"pickle protocol"):  # noqa: SIM117
-                with Bounce64(protocol) as b:
-                    pass
-        else:
-            with Bounce64(protocol) as b:
-                a, k = b.bounce(*args, **kwargs)
-                assert a == args
-                assert k == kwargs
+    with pytest.raises(ValueError, match=r"pickle protocol"):  # noqa: SIM117
+        with Bounce64(6) as b:
+            pass
