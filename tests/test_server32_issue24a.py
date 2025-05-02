f3bef593b49349168a8f82ea24c6787368f8d899
jborbely <joe.borbely@gmail.com>
2025-05-03 08:13:28 +1200

    fmt/lint test_server32_issue24a.py

--- tests/test_server32_issue24a.py
+++ tests/test_server32_issue24a.py
@@ -1,25 +1,26 @@
-from msl.loadlib import Client64
-from msl.loadlib import ConnectionTimeoutError
-from msl.loadlib import Server32
+from msl.loadlib import Client64, ConnectionTimeoutError, Server32
 
 if Server32.is_interpreter():
     # Simulate the case where importing this module on the 32-bit server hangs
-    print("importing time")
+    print("importing time")  # noqa: T201
     import time
 
-    print("sleeping for 999 seconds")
-    time.sleep(999)
+    print("sleeping for 999 seconds")  # noqa: T201
+    for _ in range(999):
+        time.sleep(1)
 
 import pytest
+
 from conftest import skipif_no_server32
 
 
 @skipif_no_server32
-def test_importing():
+def test_importing() -> None:
     class Issue24(Client64):
-        def __init__(self):
+        def __init__(self) -> None:
             super().__init__(__file__, timeout=2)
 
-    with pytest.warns(UserWarning, match=r"killed the 32-bit server using brute force"):
+    with pytest.warns(UserWarning, match=r"killed the 32-bit server using brute force"):  # noqa: SIM117
         with pytest.raises(ConnectionTimeoutError, match=r"importing time\s+sleeping for 999 seconds"):
-            Issue24()
+            with Issue24():
+                pass
