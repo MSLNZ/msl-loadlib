22303b15e617726a09cdfdf6d6e46ee4829af87b
jborbely <joe.borbely@gmail.com>
2025-05-03 13:55:44 +1200

    fmt/lint test_server32_examples_dir.py

--- tests/test_server32_examples_dir.py
+++ tests/test_server32_examples_dir.py
@@ -1,44 +1,46 @@
 import os
+from pathlib import Path
 
 from msl.examples.loadlib import EXAMPLES_DIR
-from msl.loadlib import Client64
-from msl.loadlib import Server32
+from msl.loadlib import Client64, Server32
 
 if Server32.is_interpreter():
+    from unittest.mock import Mock
 
-    def skipif_no_server32(*args):
-        pass
+    skipif_no_server32 = Mock()
 else:
-    from conftest import skipif_no_server32
+    from conftest import skipif_no_server32  # type: ignore[assignment]
 
 
 class Ex32(Server32):
-    def __init__(self, host, port, **kwargs):
+    def __init__(self, host: str, port: int) -> None:
         # this class would not instantiate if Server32.examples_dir()
         # was incorrect, so the fact that the server starts already
         # demonstrates that this test passes
-        path = os.path.join(Server32.examples_dir(), "cpp_lib32")
+        path = os.path.join(Server32.examples_dir(), "cpp_lib32")  # noqa: PTH118
         super().__init__(path, "cdll", host, port)
 
-    def ex_dir(self):
+    def ex_dir(self) -> Path:
         return self.examples_dir()
 
 
 class Ex64(Client64):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(__file__)
 
-    def examples_dir(self):
-        return self.request32("examples_dir")
+    def examples_dir(self) -> str:
+        reply: str = self.request32("examples_dir")
+        return reply
 
-    def ex_dir(self):
-        return self.request32("ex_dir")
+    def ex_dir(self) -> str:
+        reply: str = self.request32("ex_dir")
+        return reply
 
 
 @skipif_no_server32
-def test_examples_dir():
+def test_examples_dir() -> None:  # type: ignore[misc]
     assert Server32.examples_dir() == EXAMPLES_DIR
 
-    e = Ex64()
-    assert e.examples_dir() == str(EXAMPLES_DIR)
-    assert e.ex_dir() == str(EXAMPLES_DIR)
+    with Ex64() as e:
+        assert e.examples_dir() == str(EXAMPLES_DIR)
+        assert e.ex_dir() == str(EXAMPLES_DIR)
