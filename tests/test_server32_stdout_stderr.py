ac97323d6f99c2cf5db5464f70fa8907bf7c2904
jborbely <joe.borbely@gmail.com>
2025-05-03 14:34:30 +1200

    fmt/lint test_server32_stdout_stderr.py

--- tests/test_server32_stdout_stderr.py
+++ tests/test_server32_stdout_stderr.py
@@ -1,45 +1,43 @@
-from __future__ import print_function
-
 import os
 import sys
 
-from msl.loadlib import Client64
-from msl.loadlib import Server32
+from msl.loadlib import Client64, Server32
 
 if Server32.is_interpreter():
+    from unittest.mock import Mock
 
-    def skipif_no_server32(*args):
-        pass
+    skipif_no_server32 = Mock()
 else:
-    from conftest import skipif_no_server32
+    from conftest import skipif_no_server32  # type: ignore[assignment]
 
 
 class Print32(Server32):
-    def __init__(self, host, port, **kwargs):
-        path = os.path.join(Server32.examples_dir(), "cpp_lib32")
+    def __init__(self, host: str, port: int, **kwargs: str) -> None:
+        path = os.path.join(Server32.examples_dir(), "cpp_lib32")  # noqa: PTH118
         super().__init__(path, "cdll", host, port)
 
         if kwargs["show"] == "True":
-            print("this is a message")
-            print("there is a problem", file=sys.stderr)
+            print("this is a message")  # noqa: T201
+            print("there is a problem", file=sys.stderr)  # noqa: T201
 
-    def write(self, n, stdout):
+    def write(self, n: int, *, stdout: bool) -> bool:
         stream = sys.stdout if stdout else sys.stderr
         print("x" * n, end="", file=stream)
         return True
 
 
 class Print64(Client64):
-    def __init__(self, show):
+    def __init__(self, *, show: bool) -> None:
         super().__init__(__file__, show=show)
 
-    def write(self, n, stdout):
-        return self.request32("write", n, stdout)
+    def write(self, n: int, *, stdout: bool) -> bool:
+        reply: bool = self.request32("write", n, stdout=stdout)
+        return reply
 
 
 @skipif_no_server32
-def test_shutdown_server32():
-    p = Print64(True)
+def test_shutdown_server32() -> None:  # type: ignore[misc]
+    p = Print64(show=True)
     stdout, stderr = p.shutdown_server32()
     assert stdout.read().rstrip() == b"this is a message"
     assert stderr.read().rstrip() == b"there is a problem"
@@ -53,12 +51,12 @@ def test_shutdown_server32():
 
 
 @skipif_no_server32
-def test_buffer_size_4096():
+def test_buffer_size_4096() -> None:  # type: ignore[misc]
     # 4096 is the maximum buffer size for a PIPE before the 32-bit server blocks
     n = 4096
-    p = Print64(False)
-    assert p.write(n, True)
-    assert p.write(n, False)
+    p = Print64(show=False)
+    assert p.write(n, stdout=True)
+    assert p.write(n, stdout=False)
     stdout, stderr = p.shutdown_server32()
     assert stdout.read() == b"x" * n
     assert stderr.read() == b"x" * n
