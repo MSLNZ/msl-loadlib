e17b3666c138cf6bc1f9f8d4e7e1cc86e9cd16f8
jborbely <joe.borbely@gmail.com>
2025-05-03 14:24:14 +1200

    fmt/lint test_server32_rpc_timeout.py

--- tests/test_server32_rpc_timeout.py
+++ tests/test_server32_rpc_timeout.py
@@ -1,57 +1,62 @@
 import os
 import time
 
-from msl.loadlib import Client64
-from msl.loadlib import ResponseTimeoutError
-from msl.loadlib import Server32
+from msl.loadlib import Client64, ResponseTimeoutError, Server32
 
 if Server32.is_interpreter():
+    from unittest.mock import Mock
 
-    def skipif_no_server32(*args):
-        pass
+    skipif_no_server32 = Mock()
 else:
-    import pytest
-    from conftest import skipif_no_server32
+    from conftest import skipif_no_server32  # type: ignore[assignment]
 
 
 RPC_TIMEOUT = 5.0
 
 
 class RPCServer(Server32):
-    def __init__(self, host, port):
-        path = os.path.join(Server32.examples_dir(), "cpp_lib32")
+    def __init__(self, host: str, port: int) -> None:
+        path = os.path.join(Server32.examples_dir(), "cpp_lib32")  # noqa: PTH118
         super().__init__(path, "cdll", host, port)
 
-    def no_delay(self, a, b):
-        return self.lib.add(a, b)
+    def no_delay(self, a: int, b: int) -> int:
+        result: int = self.lib.add(a, b)
+        return result
 
-    def short_delay(self, a, b):
+    def short_delay(self, a: int, b: int) -> int:
         time.sleep(RPC_TIMEOUT / 2.0)
-        return self.lib.add(a, b)
+        result: int = self.lib.add(a, b)
+        return result
 
-    def long_delay(self, a, b):
+    def long_delay(self, a: int, b: int) -> int:
         time.sleep(RPC_TIMEOUT * 2.0)
-        return self.lib.add(a, b)
+        result: int = self.lib.add(a, b)
+        return result
 
 
 class RPCClient(Client64):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(__file__, rpc_timeout=RPC_TIMEOUT)
 
-    def no_delay(self, a, b):
-        return self.request32("no_delay", a, b)
+    def no_delay(self, a: int, b: int) -> int:
+        reply: int = self.request32("no_delay", a, b)
+        return reply
 
-    def short_delay(self, a, b):
-        return self.request32("short_delay", a, b)
+    def short_delay(self, a: int, b: int) -> int:
+        reply: int = self.request32("short_delay", a, b)
+        return reply
 
-    def long_delay(self, a, b):
-        return self.request32("long_delay", a, b)
+    def long_delay(self, a: int, b: int) -> int:
+        reply: int = self.request32("long_delay", a, b)
+        return reply
 
 
 @skipif_no_server32
-def test_rpc_timeout():
-    c = RPCClient()
-    assert c.no_delay(8, 3) == 11
-    assert c.short_delay(-4, 2) == -2
-    with pytest.raises(ResponseTimeoutError):
-        c.long_delay(1, 2)
+def test_rpc_timeout() -> None:  # type: ignore[misc]
+    import pytest
+
+    with RPCClient() as c:
+        assert c.no_delay(8, 3) == 11
+        assert c.short_delay(-4, 2) == -2
+        with pytest.raises(ResponseTimeoutError):
+            _ = c.long_delay(1, 2)
