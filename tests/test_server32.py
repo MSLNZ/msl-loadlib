3272ae81176cd1305e4aef60f8c21010dc1df35b
jborbely <joe.borbely@gmail.com>
2025-05-03 13:49:52 +1200

    fmt/lint test_server32.py

--- tests/test_server32.py
+++ tests/test_server32.py
@@ -1,28 +1,27 @@
+# pyright reports a reportUnknownMemberType warning for pytest.approx
+# pyright: reportUnknownMemberType=false
+from __future__ import annotations
+
 import math
 import os
-from typing import Optional
+from pathlib import Path
+from typing import Any, Callable
 
 import pytest
 
-from conftest import skipif_no_server32, IS_MAC, IS_WINDOWS
-from conftest import skipif_not_windows
+from conftest import IS_MAC, IS_WINDOWS, skipif_no_server32, skipif_not_windows
 from msl import loadlib
-from msl.examples.loadlib import Cpp64
-from msl.examples.loadlib import DotNet64
-from msl.examples.loadlib import Echo64
-from msl.examples.loadlib import Fortran64
-from msl.examples.loadlib import FourPoints
-from msl.examples.loadlib import Point
+from msl.examples.loadlib import Cpp64, DotNet64, Echo64, Fortran64, FourPoints, Point
 from msl.loadlib import ConnectionTimeoutError
 
-c: Optional[Cpp64] = None
-f: Optional[Fortran64] = None
-e: Optional[Echo64] = None
-n: Optional[DotNet64] = None
+c: Cpp64 | None = None
+f: Fortran64 | None = None
+e: Echo64 | None = None
+n: DotNet64 | None = None
 
 
-def setup_module():
-    global c, f, e, n
+def setup_module() -> None:
+    global c, f, e, n  # noqa: PLW0603
     if IS_MAC:  # the 32-bit server for macOS does not exist
         return
     c = Cpp64()
@@ -38,18 +37,21 @@ def setup_module():
             pytest.xfail("flaky test with .NET, Windows and GA")
 
 
-def teardown_module():
+def teardown_module() -> None:
     if IS_MAC:  # the 32-bit server for macOS does not exist
         return
-    c.shutdown_server32()
-    f.shutdown_server32()
-    e.shutdown_server32()
+    assert c is not None
+    assert f is not None
+    assert e is not None
+    _ = c.shutdown_server32()
+    _ = f.shutdown_server32()
+    _ = e.shutdown_server32()
     if n is not None:
-        n.shutdown_server32()
+        _ = n.shutdown_server32()
 
 
 @skipif_no_server32
-def test_unique_ports():
+def test_unique_ports() -> None:
     for obj1 in [c, f, e, n]:
         for obj2 in [c, f, e, n]:
             if obj1 is None or obj2 is None or obj1 is obj2:
@@ -58,38 +60,41 @@ def test_unique_ports():
 
 
 @skipif_no_server32
-def test_lib_name():
-    def get_name(path):
-        return os.path.basename(path).split(".")[0]
+def test_lib_name() -> None:
+    def get_name(path: str) -> str:
+        return Path(path).name.split(".")[0]
 
-    assert "cpp_lib32" == get_name(c.lib32_path)
-    assert "fortran_lib32" == get_name(f.lib32_path)
+    assert c is not None
+    assert f is not None
+    assert get_name(c.lib32_path) == "cpp_lib32"
+    assert get_name(f.lib32_path) == "fortran_lib32"
     if n is not None:
-        assert "dotnet_lib32" == get_name(n.lib32_path)
+        assert get_name(n.lib32_path) == "dotnet_lib32"
 
 
 @skipif_no_server32
-def test_server_version():
+def test_server_version() -> None:
     assert loadlib.Server32.version().startswith("Python")
 
 
 @skipif_no_server32
-def test_cpp():
-    assert 3 == c.add(1, 2)
-    assert -1002 == c.add(-1000, -2)
-    assert 10.0 == pytest.approx(c.subtract(20.0, 10.0))
-    assert -10.0 == pytest.approx(c.subtract(90.0, 100.0))
-    assert 0.0 == pytest.approx(c.add_or_subtract(0.1234, -0.1234, do_addition=True))
-    assert 100.0 == pytest.approx(c.add_or_subtract(123.456, 23.456, do_addition=False))
+def test_cpp() -> None:
+    assert c is not None
+    assert c.add(1, 2) == 3
+    assert c.add(-1000, -2) == -1002
+    assert pytest.approx(c.subtract(20.0, 10.0)) == 10.0
+    assert pytest.approx(c.subtract(90.0, 100.0)) == -10.0
+    assert pytest.approx(c.add_or_subtract(0.1234, -0.1234, do_addition=True)) == 0.0
+    assert pytest.approx(c.add_or_subtract(123.456, 23.456, do_addition=False)) == 100.0
 
     a = 3.1415926
     values = [float(x) for x in range(100)]
     c_values = c.scalar_multiply(a, values)
     for i in range(len(values)):
-        assert a * values[i] == pytest.approx(c_values[i])
+        assert pytest.approx(c_values[i]) == a * values[i]
 
-    assert "0987654321" == c.reverse_string_v1("1234567890")
-    assert "[abc x|z j 1 *&" == c.reverse_string_v2("&* 1 j z|x cba[")
+    assert c.reverse_string_v1("1234567890") == "0987654321"
+    assert c.reverse_string_v2("&* 1 j z|x cba[") == "[abc x|z j 1 *&"
 
     fp = FourPoints(Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0))
     assert c.distance_4_points(fp) == 4.0
@@ -101,40 +106,42 @@ def test_cpp():
 
 
 @skipif_no_server32
-def test_fortran():
-    assert -127 == f.sum_8bit(-(2**7), 1)
-    assert 32766 == f.sum_16bit(2**15 - 1, -1)
-    assert 123456789 == f.sum_32bit(123456788, 1)
-    assert -9223372036854775807 == f.sum_64bit(-(2**63), 1)
-    assert -52487.570494 == pytest.approx(f.multiply_float32(40.874, -1284.131))
-    assert 2.31e300 == pytest.approx(f.multiply_float64(1.1e100, 2.1e200))
+def test_fortran() -> None:
+    assert f is not None
+    assert f.sum_8bit(-(2**7), 1) == -127
+    assert f.sum_16bit(2**15 - 1, -1) == 32766
+    assert f.sum_32bit(123456788, 1) == 123456789
+    assert f.sum_64bit(-(2**63), 1) == -9223372036854775807
+    assert pytest.approx(f.multiply_float32(40.874, -1284.131)) == -52487.570494
+    assert pytest.approx(f.multiply_float64(1.1e100, 2.1e200)) == 2.31e300
     assert f.is_positive(1e-100)
     assert not f.is_positive(-1e-100)
-    assert 3000 == f.add_or_subtract(1000, 2000, do_addition=True)
-    assert -1000 == f.add_or_subtract(1000, 2000, do_addition=False)
-    assert 1 == int(f.factorial(0))
-    assert 1 == int(f.factorial(1))
-    assert 120 == int(f.factorial(5))
-    assert 2.73861278752583 == pytest.approx(f.standard_deviation([float(val) for val in range(1, 10)]))
-    assert 0.171650807137 == pytest.approx(f.besselJ0(8.0))
-    assert "!dlrow olleh" == f.reverse_string("hello world!")
+    assert f.add_or_subtract(1000, 2000, do_addition=True) == 3000
+    assert f.add_or_subtract(1000, 2000, do_addition=False) == -1000
+    assert int(f.factorial(0)) == 1
+    assert int(f.factorial(1)) == 1
+    assert int(f.factorial(5)) == 120
+    assert pytest.approx(f.standard_deviation([float(val) for val in range(1, 10)])) == 2.73861278752583
+    assert pytest.approx(f.besselJ0(8.0)) == 0.171650807137
+    assert f.reverse_string("hello world!") == "!dlrow olleh"
 
     a = [float(val) for val in range(1, 1000)]
     b = [3.0 * val for val in range(1, 1000)]
     f_values = f.add_1d_arrays(a, b)
     for i in range(len(a)):
-        assert a[i] + b[i] == pytest.approx(f_values[i])
+        assert pytest.approx(f_values[i]) == a[i] + b[i]
 
     f_mat = f.matrix_multiply([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-    assert 22.0 == pytest.approx(f_mat[0][0])
-    assert 28.0 == pytest.approx(f_mat[0][1])
-    assert 49.0 == pytest.approx(f_mat[1][0])
-    assert 64.0 == pytest.approx(f_mat[1][1])
+    assert pytest.approx(f_mat[0][0]) == 22.0
+    assert pytest.approx(f_mat[0][1]) == 28.0
+    assert pytest.approx(f_mat[1][0]) == 49.0
+    assert pytest.approx(f_mat[1][1]) == 64.0
 
 
 @skipif_no_server32
-def test_echo():
-    args, kwargs = e.send_data(True)
+def test_echo() -> None:
+    assert e is not None
+    args, kwargs = e.send_data(True)  # noqa: FBT003
     assert args[0]
     assert isinstance(args[0], bool)
     assert not kwargs
@@ -143,7 +150,7 @@ def test_echo():
     assert not args
     assert kwargs == {"x": 1.0}
 
-    x = [val for val in range(100)]
+    x = list(range(100))
     y = range(9999)
     my_dict = {"x": x, "y": y, "text": "abcd 1234 wxyz"}  # cSpell: ignore wxyz
     args, kwargs = e.send_data(111, 2.3, complex(-1.2, 2.30), (1, 2), x=x, y=y, my_dict=my_dict)
@@ -157,7 +164,8 @@ def test_echo():
 
 
 @skipif_not_windows
-def test_dotnet():
+def test_dotnet() -> None:
+    assert n is not None
     names = n.get_class_names()
     assert len(names) == 4
     assert "StringManipulation" in names
@@ -165,71 +173,73 @@ def test_dotnet():
     assert "DotNetMSL.ArrayManipulation" in names
     assert "StaticClass" in names
 
-    assert 9 == n.add_integers(4, 5)
-    assert 0.8 == pytest.approx(n.divide_floats(4.0, 5.0))
-    assert 458383.926 == pytest.approx(n.multiply_doubles(872.24, 525.525))
-    assert 108.0 == pytest.approx(n.add_or_subtract(99.0, 9.0, do_addition=True))
-    assert 90.0 == pytest.approx(n.add_or_subtract(99.0, 9.0, do_addition=False))
+    assert n.add_integers(4, 5) == 9
+    assert pytest.approx(n.divide_floats(4.0, 5.0)) == 0.8
+    assert pytest.approx(n.multiply_doubles(872.24, 525.525)) == 458383.926
+    assert pytest.approx(n.add_or_subtract(99.0, 9.0, do_addition=True)) == 108.0
+    assert pytest.approx(n.add_or_subtract(99.0, 9.0, do_addition=False)) == 90.0
 
     a = 7.13141
     values = [float(x) for x in range(1000)]
     net_values = n.scalar_multiply(a, values)
     for i in range(len(values)):
-        assert a * values[i] == pytest.approx(net_values[i])
+        assert pytest.approx(net_values[i]) == a * values[i]
 
     assert n.reverse_string("New Zealand") == "dnalaeZ weN"
 
     net_mat = n.multiply_matrices([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-    assert 22.0 == pytest.approx(net_mat[0][0])
-    assert 28.0 == pytest.approx(net_mat[0][1])
-    assert 49.0 == pytest.approx(net_mat[1][0])
-    assert 64.0 == pytest.approx(net_mat[1][1])
+    assert pytest.approx(net_mat[0][0]) == 22.0
+    assert pytest.approx(net_mat[0][1]) == 28.0
+    assert pytest.approx(net_mat[1][0]) == 49.0
+    assert pytest.approx(net_mat[1][1]) == 64.0
 
-    assert 33 == n.add_multiple(11, -22, 33, -44, 55)
-    assert "the experiment worked " == n.concatenate("the ", "experiment ", "worked ", False, "temporarily")
-    assert "the experiment worked temporarily" == n.concatenate("the ", "experiment ", "worked ", True, "temporarily")
+    assert n.add_multiple(11, -22, 33, -44, 55) == 33
+    assert n.concatenate("the ", "experiment ", "worked ", False, "temporarily") == "the experiment worked "  # noqa: FBT003
+    assert n.concatenate("the ", "experiment ", "worked ", True, "temporarily") == "the experiment worked temporarily"  # noqa: FBT003
 
 
 @skipif_no_server32
-def test_unicode_path():
+def test_unicode_path() -> None:
     class Cpp64Encoding(loadlib.Client64):
-        def __init__(self):
-            dir_name = os.path.dirname(__file__)
+        def __init__(self) -> None:
+            dir_name = Path(__file__).parent
             super().__init__(
-                module32="cpp32unicode",
+                "cpp32unicode",
                 append_sys_path=f"{dir_name}/uñicödé",
                 append_environ_path=f"{dir_name}/uñicödé",
             )
 
-        def add(self, a, b):
-            return self.request32("add", a, b)
+        def add(self, a: int, b: int) -> int:
+            out: int = self.request32("add", a, b)
+            return out
 
     c2 = Cpp64Encoding()
     assert c2.add(-5, 3) == -2
 
     with pytest.raises(loadlib.Server32Error):
-        c2.add("hello", "world")
+        _ = c2.add("hello", "world")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
 
     try:
-        c2.add("hello", "world")
+        _ = c2.add("hello", "world")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
     except loadlib.Server32Error as err:
-        print(err)  # must not raise an error
+        assert isinstance(str(err), str)  # noqa: PT017
 
-    c2.shutdown_server32()
+    _ = c2.shutdown_server32()
 
 
 @skipif_no_server32
-def test_server32_error():
+def test_server32_error() -> None:
+    assert c is not None
     try:
-        c.add("hello", "world")
+        _ = c.add("hello", "world")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
     except loadlib.Server32Error as err:
-        assert err.name == "ArgumentError"
-        assert "argument 1: TypeError:" in err.value
-        assert err.traceback.endswith("result: int = self.lib.add(a, b)")
+        assert err.name == "ArgumentError"  # noqa: PT017
+        assert "argument 1: TypeError:" in err.value  # noqa: PT017
+        assert err.traceback.endswith("result: int = self.lib.add(a, b)")  # noqa: PT017
 
 
 @skipif_not_windows
-def test_comtypes_ctypes_union_error():
+def test_comtypes_ctypes_union_error() -> None:
     # Changes to ctypes in Python 3.7.6 and 3.8.1 caused the following exception
     #   TypeError: item 1 in _argtypes_ passes a union by value, which is unsupported.
     # when loading some COM objects, see https://bugs.python.org/issue16575
@@ -238,75 +248,79 @@ def test_comtypes_ctypes_union_error():
     # is running on does not raise this TypeError
 
     class FileSystemObjectClient(loadlib.Client64):
-        def __init__(self):
+        def __init__(self) -> None:
             super().__init__(
-                module32="ctypes_union_error",
-                append_sys_path=os.path.join(os.path.dirname(__file__), "server32_comtypes"),
+                "ctypes_union_error",
+                append_sys_path=Path(__file__).parent / "server32_comtypes",
             )
 
-        def __getattr__(self, name):
-            def send(*args, **kwargs):
+        def __getattr__(self, name: str) -> Callable[..., Any]:
+            def send(*args: Any, **kwargs: Any) -> Any:
                 return self.request32(name, *args, **kwargs)
 
             return send
 
     file_system = FileSystemObjectClient()
     file_system.create_and_write("foo<bar<baz>>")
-    temp_file = file_system.get_temp_file()
 
-    with open(temp_file, mode="rt") as fp:
-        source = fp.read().strip()
-
+    temp_file = Path(file_system.get_temp_file())
+    source = temp_file.read_text().strip()
     assert source == "foo<bar<baz>>"
+    temp_file.unlink()
 
-    os.remove(temp_file)
-    file_system.shutdown_server32()
+    _ = file_system.shutdown_server32()
 
 
 @skipif_not_windows
-def test_comtypes_shell32():
+def test_comtypes_shell32() -> None:
     class Shell64(loadlib.Client64):
-        def __init__(self):
+        def __init__(self) -> None:
             super().__init__(
-                module32="shell32.py",
-                append_sys_path=os.path.join(os.path.dirname(__file__), "server32_comtypes"),
+                "shell32.py",
+                append_sys_path=Path(__file__).parent / "server32_comtypes",
             )
 
-        def environ(self, key):
-            return self.request32("environ", key)
+        def environ(self, key: str) -> str:
+            value: str = self.request32("environ", key)
+            return value
 
     shell = Shell64()
 
     for name in ["PROCESSOR_IDENTIFIER", "NUMBER_OF_PROCESSORS"]:
         assert shell.environ(name) == os.environ[name]
 
-    shell.shutdown_server32()
+    _ = shell.shutdown_server32()
 
 
 @skipif_not_windows
-def test_activex():
+def test_activex() -> None:
     class ActiveX(loadlib.Client64):
-        def __init__(self):
+        def __init__(self) -> None:
             super().__init__(
-                module32="activex_media_player.py",
-                append_sys_path=os.path.join(os.path.dirname(__file__), "server32_comtypes"),
+                "activex_media_player.py",
+                append_sys_path=Path(__file__).parent / "server32_comtypes",
                 timeout=30,
             )
 
-        def this(self):
-            return self.request32("this")
+        def this(self) -> bool:
+            reply: bool = self.request32("this")
+            return reply
 
-        def reload(self):
-            return self.request32("reload")
+        def reload(self) -> bool:
+            reply: bool = self.request32("reload")
+            return reply
 
-        def load_library(self):
-            return self.request32("load_library")
+        def load_library(self) -> bool:
+            reply: bool = self.request32("load_library")
+            return reply
 
-        def error1(self):
-            return self.request32("error1")
+        def error1(self) -> str:
+            reply: str = self.request32("error1")
+            return reply
 
-        def error2(self):
-            return self.request32("error2")
+        def error2(self) -> str:
+            reply: str = self.request32("error2")
+            return reply
 
     ax = ActiveX()
 
