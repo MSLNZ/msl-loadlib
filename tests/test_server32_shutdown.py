41bccce1b5cd62d8111f64bdf83a51ef3959ecc5
jborbely <joe.borbely@gmail.com>
2025-05-03 14:28:31 +1200

    fmt/lint test_server32_shutdown.py

--- tests/test_server32_shutdown.py
+++ tests/test_server32_shutdown.py
@@ -1,44 +1,46 @@
 import os
 import time
 
-from msl.loadlib import Client64
-from msl.loadlib import Server32
+from msl.loadlib import Client64, Server32
 
 if Server32.is_interpreter():
+    from unittest.mock import Mock
 
-    def skipif_no_server32(*args):
-        pass
+    skipif_no_server32 = Mock()
 else:
-    import pytest
-    from conftest import skipif_no_server32
+    from conftest import skipif_no_server32  # type: ignore[assignment]
 
 
 class ShutdownHangs(Server32):
-    def __init__(self, host, port):
-        path = os.path.join(Server32.examples_dir(), "cpp_lib32")
+    def __init__(self, host: str, port: int) -> None:
+        path = os.path.join(Server32.examples_dir(), "cpp_lib32")  # noqa: PTH118
         super().__init__(path, "cdll", host, port)
 
-    def add(self, x, y):
-        return self.lib.add(x, y)
+    def add(self, x: int, y: int) -> int:
+        result: int = self.lib.add(x, y)
+        return result
 
-    def shutdown_handler(self):
+    def shutdown_handler(self) -> None:
         time.sleep(999)
 
 
 @skipif_no_server32
-def test_killed():
+def test_killed() -> None:  # type: ignore[misc]
+    import pytest
+
     class Hangs(Client64):
-        def __init__(self):
+        def __init__(self) -> None:
             super().__init__(__file__)
 
-        def add(self, x, y):
-            return self.request32("add", x, y)
+        def add(self, x: int, y: int) -> int:
+            reply: int = self.request32("add", x, y)
+            return reply
 
-    hangs = Hangs()
-    assert hangs.add(1, 1) == 2
-    with pytest.warns(UserWarning, match=r"killed the 32-bit server using brute force") as warn_info:
-        hangs.shutdown_server32(kill_timeout=2)
+    with Hangs() as hangs:
+        assert hangs.add(1, 1) == 2
+        with pytest.warns(UserWarning, match=r"killed the 32-bit server using brute force") as warn_info:
+            _ = hangs.shutdown_server32(kill_timeout=2)
 
-    assert len(warn_info.list) == 1
-    assert warn_info.list[0].filename == __file__
-    assert warn_info.list[0].lineno == 40  # occurs at hangs.shutdown_server32 above
+        assert len(warn_info.list) == 1
+        assert warn_info.list[0].filename == __file__
+        assert warn_info.list[0].lineno == 42  # occurs at hangs.shutdown_server32 above
