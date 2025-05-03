a2bcc1834d78c5f25cf8e9adfe60b6958b63480b
jborbely <joe.borbely@gmail.com>
2025-05-03 13:49:10 +1200

    move the bad_server Client into test_server32_bad_modules.py

--- tests/bad_servers/client.py
+++
@@ -1,6 +0,0 @@
-from msl.loadlib import Client64
-
-
-class Client(Client64):
-    def __init__(self, module32: str) -> None:
-        super().__init__(module32, timeout=5)
--- tests/test_server32_bad_modules.py
+++ tests/test_server32_bad_modules.py
@@ -1,16 +1,17 @@
-import os
-import sys
+from pathlib import Path
 
 import pytest
 
 from conftest import skipif_no_server32
-from msl.loadlib import ConnectionTimeoutError
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "bad_servers"))
-from client import Client
+from msl.loadlib import Client64, ConnectionTimeoutError
 
 
-def check(module_name, match):
+class Client(Client64):
+    def __init__(self, module32: str) -> None:
+        super().__init__(module32, append_sys_path=Path(__file__).parent / "bad_servers", timeout=5)
+
+
+def check(module_name: str, match: str) -> None:
     # This test module is a bit buggy.
     # Sometimes we get a ConnectionRefusedError that we want to ignore.
     attempts = 1
@@ -19,8 +20,8 @@ def check(module_name, match):
     while True:
         try:
             with pytest.raises(ConnectionTimeoutError, match=match):
-                Client(module_name)
-        except ConnectionRefusedError:
+                _ = Client(module_name)
+        except ConnectionRefusedError:  # noqa: PERF203
             if attempts == max_attempts:
                 raise
             attempts += 1
@@ -29,81 +30,81 @@ def check(module_name, match):
 
 
 @skipif_no_server32
-def test_no_module():
+def test_no_module() -> None:
     check("", r"specify a Python module")
 
 
 @skipif_no_server32
-def test_relative_import():
+def test_relative_import() -> None:
     check(".relative", r"Cannot perform relative imports")
 
 
 @skipif_no_server32
-def test_import_error():
+def test_import_error() -> None:
     # the module import_error.py exists, but the module imports a package that does not exist
     check("import_error", r"No module named 'missing'")
 
 
 @skipif_no_server32
-def test_import_error2():
+def test_import_error2() -> None:
     check("doesnotexist", r"module must be in sys.path")
 
 
 @skipif_no_server32
-def test_no_server32_subclass():
+def test_no_server32_subclass() -> None:
     check("no_server32_subclass", r"Module does not contain a class that is a subclass of Server32")
 
 
 @skipif_no_server32
-def test_no_init():
+def test_no_init() -> None:
     check("no_init", r"class NoInit\(Server32\):")
 
 
 @skipif_no_server32
-def test_bad_init_args():
+def test_bad_init_args() -> None:
     check("bad_init_args", r"class BadInitArgs\(Server32\):")
 
 
 @skipif_no_server32
-def test_bad_init_args2():
+def test_bad_init_args2() -> None:
     check("bad_init_args2", r"missing 1 required positional argument: 'extra'")
 
 
 @skipif_no_server32
-def test_no_super():
+def test_no_super() -> None:
     check("no_super", r"class NoSuper\(Server32\):")
 
 
 @skipif_no_server32
-def test_bad_super_init():
+def test_bad_super_init() -> None:
     check("bad_super_init", r"class BadSuperInit\(Server32\):")
 
 
 @skipif_no_server32
-def test_bad_lib_path():
+def test_bad_lib_path() -> None:
     check("bad_lib_path", r"Cannot find 'doesnotexist' for libtype='cdll'")
 
 
 @skipif_no_server32
-def test_bad_lib_type():
+def test_bad_lib_type() -> None:
     check("bad_lib_type", r"ValueError: Invalid libtype 'invalid'")
 
 
 @skipif_no_server32
-def test_unexpected_error():
+def test_unexpected_error() -> None:
     check("unexpected_error", r"ZeroDivisionError")
 
 
 @skipif_no_server32
-def test_unexpected_error2():
+def test_unexpected_error2() -> None:
     check("unexpected_error2", r"TypeError: unsupported operand")
 
 
 @skipif_no_server32
-def test_unexpected_error3():
+def test_unexpected_error3() -> None:
     check("unexpected_error3", r"NameError: name 'y' is not defined")
 
 
 @skipif_no_server32
-def test_wrong_bitness():
+def test_wrong_bitness() -> None:
     check("wrong_bitness", r"Failed to load")
